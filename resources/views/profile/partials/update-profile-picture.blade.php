f3df2b6d4cfbe98c3ccfe436c5089b5209b1fd59
Sehansel <sebastianparali711@gmail.com>
2024-09-30 14:26:38 +0700

    first commit

---
+++ resources/views/profile/partials/update-profile-picture.blade.php
@@ -0,0 +1,32 @@
+<section>
+    <header>
+        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+            {{ __('Update Profile Picture') }}
+        </h2>
+
+        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+            {{ __('Add or edit your profile picture here') }}
+        </p>
+    </header>
+    <form method="POST" action="{{ route('profile.update') }}" class="mt-6 space-y-6"> <!-- need to change route -->
+        @csrf
+        @method('PUT')
+        <div>
+            <x-input-label for="update_password_password_confirmation" :value="__('Profile Photo')" />
+            <x-text-input id="update_profile_photo" name="profile_photo" type="file" class="mt-1 block w-full"/>
+        </div>
+        <div class="flex items-center gap-4">
+            <x-primary-button>{{ __('Save') }}</x-primary-button>
+
+            @if (session('status') === 'profile-updated')
+                <p
+                    x-data="{ show: true }"
+                    x-show="show"
+                    x-transition
+                    x-init="setTimeout(() => show = false, 2000)"
+                    class="text-sm text-gray-600 dark:text-gray-400"
+                >{{ __('Saved.') }}</p>
+            @endif
+        </div>
+    </form>
+</section>
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+Route::get('/', function () {
+    return redirect()->route('register');
+});
+
+Route::get('/dashboard', function () {
+    if(Auth::check()){
+        return view('dashboard');
+    }else{
+        return view('/');
+    }
+    
+})->middleware(['auth', 'verified', 'prevent-back-history', 'redirect-if-logged-out'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+Route::get('admin/dashboard', [HomeController::class, 'index']) -> middleware(['auth', 'admin', 'prevent-back-history']);
